4cfedea6d8e40d72566ac92aa77dd003e7d1747a
Vu Hoang Tien <vht.20021020@gmail.com>
2024-12-17 09:15:34 +0700

    fix(feature/user): fix edit user

--- src/screens/User/Profile.tsx
+++ src/screens/User/Profile.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import {
 	Alert,
 	Button,
@@ -9,19 +8,19 @@ import {
 	Row,
 } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { RootState } from "~/store/store";
-import ResetPassword from "./ResetPassword";
-import ProfileEdit from "./ProfileEdit";
 
 const Profile = () => {
 	const { error, user } = useSelector((state: RootState) => state.auth);
-	const [isEditing, setIsEditing] = useState(false);
-	const [isChangingPassword, setIsChangingPassword] = useState(false);
+	// const [isEditing, setIsEditing] = useState(false);
+	// const [isChangingPassword, setIsChangingPassword] = useState(false);
+	const navigate = useNavigate();
 
-	const handleBack = () => {
-		setIsChangingPassword(false);
-		setIsEditing(false);
-	};
+	// const handleBack = () => {
+	// 	setIsChangingPassword(false);
+	// 	setIsEditing(false);
+	// };
 
 	if (!user?.id) {
 		return (
@@ -39,88 +38,77 @@ const Profile = () => {
 						<Card.Header as="h3">Thông Tin Cá Nhân</Card.Header>
 						<Card.Body>
 							{error && <Alert variant="danger">{error}</Alert>}
-
-							{!isEditing && !isChangingPassword ? (
-								<>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											ID:
-										</Form.Label>
-										<Col sm="8">{user.id}</Col>
-									</Form.Group>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											Tên Đăng Nhập:
-										</Form.Label>
-										<Col sm="8">{user.username}</Col>
-									</Form.Group>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											Email:
-										</Form.Label>
-										<Col sm="8">{user.email}</Col>
-									</Form.Group>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											Họ:
-										</Form.Label>
-										<Col sm="8">{user.lastName}</Col>
-									</Form.Group>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											Tên:
-										</Form.Label>
-										<Col sm="8">{user.firstName}</Col>
-									</Form.Group>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											Giới Tính:
-										</Form.Label>
-										<Col sm="8">{user.gender}</Col>
-									</Form.Group>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											Ngày Tạo:
-										</Form.Label>
-										<Col sm="8">
-											{user.createdAt
-												? new Date(user.createdAt).toLocaleString()
-												: "N/A"}
-										</Col>
-									</Form.Group>
-									<Form.Group as={Row} className="mb-3">
-										<Form.Label column sm="4">
-											Ngày Cập Nhật:
-										</Form.Label>
-										<Col sm="8">
-											{user.updatedAt
-												? new Date(user.updatedAt).toLocaleString()
-												: "N/A"}
-										</Col>
-									</Form.Group>
-									<Button
-										variant="primary"
-										onClick={() => {
-											setIsEditing(true);
-										}}
-									>
-										Chỉnh Sửa Thông Tin
-									</Button>
-									<Button
-										className="ms-2"
-										variant="success"
-										onClick={() => {
-											setIsChangingPassword(true);
-										}}
-									>
-										Thay Đổi Mật Khẩu
-									</Button>
-								</>
-							) : isEditing ? (
-								<ProfileEdit onBack={handleBack} />
-							) : (
-								<ResetPassword onBack={handleBack} />
-							)}
+							<>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										ID:
+									</Form.Label>
+									<Col sm="8">{user.id}</Col>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										Tên Đăng Nhập:
+									</Form.Label>
+									<Col sm="8">{user.username}</Col>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										Email:
+									</Form.Label>
+									<Col sm="8">{user.email}</Col>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										Họ:
+									</Form.Label>
+									<Col sm="8">{user.lastName}</Col>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										Tên:
+									</Form.Label>
+									<Col sm="8">{user.firstName}</Col>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										Giới Tính:
+									</Form.Label>
+									<Col sm="8">{user.gender}</Col>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										Ngày Tạo:
+									</Form.Label>
+									<Col sm="8">
+										{user.createdAt
+											? new Date(user.createdAt).toLocaleString()
+											: "N/A"}
+									</Col>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Form.Label column sm="4">
+										Ngày Cập Nhật:
+									</Form.Label>
+									<Col sm="8">
+										{user.updatedAt
+											? new Date(user.updatedAt).toLocaleString()
+											: "N/A"}
+									</Col>
+								</Form.Group>
+								<Button
+									variant="primary"
+									onClick={() => navigate("/profile/edit")}
+								>
+									Chỉnh Sửa Thông Tin
+								</Button>
+								<Button
+									className="ms-2"
+									variant="success"
+									onClick={() => navigate("/profile/reset-password")}
+								>
+									Thay Đổi Mật Khẩu
+								</Button>
+							</>
 						</Card.Body>
 					</Card>
 				</Col>
