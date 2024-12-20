b32733d4fc9870b129856f5e9d30a2160d7b36af
Vu Hoang Tien <vht.20021020@gmail.com>
2024-12-20 15:11:12 +0700

    fix(feature/task): fix handlers

--- src/mocks/handlers/handlers.ts
+++ src/mocks/handlers/handlers.ts
@@ -2,11 +2,10 @@ import { authHandlers } from "./authHandlers";
 import { userHandlers } from "./userHandlers";
 import { categoryHandlers } from "./categoryHandlers";
 import { taskHandlers } from "./taskHandlers";
-import { userHandlers } from "./userHandlers";
 
 export const handlers = [
 	...authHandlers,
 	...userHandlers,
 	...categoryHandlers,
 	...taskHandlers,
-, ...userHandlers];
+];
